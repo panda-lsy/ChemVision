1bef5b5a11a2d637f29221afee98a88e2d098b5a
panda-lsy <shengxia23@hainnu.edu.cn>
2026-06-13 23:13:25 +0800

    ci: 修复 Android 阿里云镜像优先级 + Release 容错
    - build.gradle.kts: google()/mavenCentral() 移到阿里云镜像之前
    - release job: 按需下载 artifact，单个构建失败不影响发布

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,16 +1,10 @@
 allprojects {
     repositories {
+        google()
+        mavenCentral()
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        google()
-        mavenCentral()
-    }
-}
-
-allprojects {
-    repositories {
-        
     }
 }
 
