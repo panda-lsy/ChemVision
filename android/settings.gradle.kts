aed0b07ee0e3b471489c3808665c6a8a2edea70b
panda-lsy <shengxia23@hainnu.edu.cn>
2026-06-13 22:53:16 +0800

    ci: 添加 dependencyResolutionManagement 修复 Android 构建
    - settings.gradle.kts: 添加 dependencyResolutionManagement 块
      google()/mavenCentral() 优先，阿里云镜像作为回退

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -20,6 +20,16 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+    }
+}
+
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.12.2" apply false
